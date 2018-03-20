49aecf1dcec93ccb63cce7b152c5e11d4f21e827
Benoit <benoit.boureau@eemi.com>
2018-03-20 09:51:29 +0100

    correction affichage nav co

--- includes/nav_co.php
+++ includes/nav_co.php
@@ -12,7 +12,7 @@ if(isset($_SESSION['user'])) {
     echo '<nav class="white nav-co">
     <div class="nav-wrapper row flex flex-a">
         <a href="index-co.php" class="brand-logo"><img class="width-40" src="images/logo.png"></a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down col s4">
+        <ul id="nav-mobile" style="width: 40%" class="right hide-on-med-and-down col s4">
             <li><a href="liste_annonces.php">Annonces</a></li>
             <li><a href="poster_annonce.php">Poster une annonce</a></li>
             <li><a href="profil.php">Mon profil</a></li>
@@ -26,7 +26,7 @@ else if(isset($_SESSION['admin'])) {
     echo '<nav class="white nav-co">
     <div class="nav-wrapper row flex flex-a">
         <a href="index-co.php" class="brand-logo"><img class="width-40" src="images/logo.png"></a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down col s4">
+        <ul id="nav-mobile" style="width: 40%" class="right hide-on-med-and-down col s4">
             <li><a href="liste_annonces.php">Annonces</a></li>
             <li><a href="profil.php">Mon profil</a></li>
             <li><a href="admin/gestion_clients.php">BackOffice</a></li>
