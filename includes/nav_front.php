8695f7061dea485d248e2019c001a313b58fd7fe
Benoit <benoit.boureau@eemi.com>
2018-03-20 10:33:33 +0100

    Corrections Liens Nav connexion
    Les liens redirigent correctement vers les sections de la homepage meme depuis la page de connexion

--- includes/nav_front.php
+++ includes/nav_front.php
@@ -2,11 +2,23 @@
     <div class="nav-wrapper row flex flex-a">
         <a href="index.php" class="brand-logo row"><img class="width-30" src="images/logo.png" alt="logo"></a>
         <ul id="nav-mobile" class="right hide-on-med-and-down col s6">
-            <li><a href="#valeurs" class="scroll js-scrollTo">Valeurs</a></li>
-            <li><a href="#fonctionnement" class="js-scrollTo">Fonctionnement</a></li>
-            <li><a href="#certifications" class="js-scrollTo">Certifications</a></li>
-            <li><a href="#temoignages" class="js-scrollTo">Témoignages</a></li>
+            <li><a href="#valeurs" class="js-scrollTo" id="vers-valeur">Valeurs</a></li>
+            <li><a href="#fonctionnement" class="js-scrollTo" id="vers-fonction">Fonctionnement</a></li>
+            <li><a href="#certifications" class="js-scrollTo" id="vers-certif">Certifications</a></li>
+            <li><a href="#temoignages" class="js-scrollTo" id="vers-temoignage">Témoignages</a></li>
             <li class="btn waves-light waves-effect blue darken-3 marg-top-2"><a href="connexion.php">Se connecter</a></li>
         </ul>
     </div>
 </nav>
+
+
+<script>
+
+    if (location.url = ("connexion.php")) {
+        document.getElementById('vers-valeur').href = "index.php#valeurs";
+        document.getElementById('vers-fonction').href = "index.php#fonctionnement";
+        document.getElementById('vers-certif').href = "index.php#certifications";
+        document.getElementById('vers-temoignage').href = "index.php#temoignages";
+    }
+
+</script>
