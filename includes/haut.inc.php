0b58bf513aac6aa1c3e97532858b4cedf2722d3a
Benoit <benoit.boureau@eemi.com>
2018-03-22 11:10:58 +0100

    tarteaucitron

--- includes/haut.inc.php
+++ includes/haut.inc.php
@@ -13,6 +13,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="icon" type="image/png" href="images/favicon.png" />
     <title><?= $title ?></title>
+    <script type="text/javascript" src="/tarteaucitron/tarteaucitron.js"></script>
+
+    <script type="text/javascript">
+        tarteaucitron.init({
+            "hashtag": "#tarteaucitron", /* Ouverture automatique du panel avec le hashtag */
+            "highPrivacy": false, /* désactiver le consentement implicite (en naviguant) ? */
+            "orientation": "top", /* le bandeau doit être en haut (top) ou en bas (bottom) ? */
+            "adblocker": false, /* Afficher un message si un adblocker est détecté */
+            "showAlertSmall": true, /* afficher le petit bandeau en bas à droite ? */
+            "cookieslist": true, /* Afficher la liste des cookies installés ? */
+            "removeCredit": false /* supprimer le lien vers la source ? */
+        });
+    </script>
 </head>
 <body>
 
